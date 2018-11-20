5d608ff5a57f832913d1e5b57154c38cd098d63d
Ornelio Reiph <44802153+oreiph28@users.noreply.github.com>
2018-11-20 14:51:40 +0100

    Add files via upload

---
+++ week2 opdracht/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="generator" content="CoffeeCup HTML Editor (www.coffeecup.com)">
+    <meta name="dcterms.created" content="di, 20 nov 2018 13:26:15 GMT">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title></title>
+    
+  </head>
+  <body>
+  
+  
+  
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+  </body>
+</html>
